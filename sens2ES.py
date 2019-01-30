2b9b3745053a3be5327f3407f80cdc06a0b4aaa0
Mäder Michael <mike@moik.org>
2019-01-30 10:14:22 +0100

    removed the old ES stuff outsourced the config stuff exception for the request call

--- sens2ES.py
+++ sens2ES.py
@@ -1,10 +1,10 @@
 import requests
 import datetime
-import time
-from pyelasticsearch import ElasticSearch
+import configparser
+import argparse
+import os
+import sys
 
-
-es = ElasticSearch('http://docker.moik.org:9200/')
 
 url_sensor = "https://www.data199.com/api/pv1/device/lastmeasurement"
 
@@ -26,17 +26,79 @@ last_rain['id']=0
 last_rain['amount']=0
 last_wind['id']=0
 
+
+
+def configSectionMap(config, section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                print("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+def parseTheArgs() -> object:
+    parser = argparse.ArgumentParser(description='Reads a value from the BME280 sensor and writes it to MQTT and DB')
+    parser.add_argument('-f', help='path and filename of the config file, default is ./config.rc',
+                        default='config.rc')
+
+    args = parser.parse_args()
+    return args
+
+
+def readConfig(config):
+    try:
+        conf_mqtt = configSectionMap(config, "MQTT")
+    except:
+        print("Could not open config file, or could not find config section in file")
+        config_full_path = os.getcwd() + "/" + args.f
+        print("Tried to open the config file: ", config_full_path)
+        raise ValueError
+    try:
+        conf_db = configSectionMap(config, "DB")
+    except:
+        print("Could not open config file, or could not find config section in file")
+        config_full_path = os.getcwd() + "/" + args.f
+        print("Tried to open the config file: ", config_full_path)
+        raise ValueError
+    try:
+        conf_alert_sensor = configSectionMap(config, "ALERT_SENSOR")
+    except:
+        print("Could not open config file, or could not find config section in file")
+        config_full_path = os.getcwd() + "/" + args.f
+        print("Tried to open the config file: ", config_full_path)
+        raise ValueError
+    return (conf_mqtt, conf_db, conf_alert_sensor)
+
+
+
 print("Starting")
-while True:
-    time.sleep(60)
+def main(cf):
 
-    response = requests.request("POST", url_sensor, data=payload_sensor, headers=headers_sensor).json()
+    try:
+        (conf_mqtt, conf_db, conf_sensor)=readConfig(cf)
+    except ValueError:
+        exit(1)
+    #    response = requests.request("POST", url_sensor, data=payload_sensor, headers=headers_sensor).json()
+    headers = {'cache-control': 'no-cache','content-type': 'application/x-www-form-urlencoded'}
+    payload = 'phoneid='+conf_sensor['phoneid']+"&deviceids="+conf_sensor['deviceids']+"&undefined="
+    url = conf_sensor['url']
+#    payload = "phoneid=789807439177&deviceids=081323E85744%2C0B250836A75B&undefined="
 
-    if response['success'] != True:
+    try:
+        response = requests.request("POST", url, data=payload, headers=headers)
+    except:
+        print("Could not connect to METEO Cloud Server. Aborting")
+        exit(1)
+    if response.status_code == 400:
         print ("problem contacting the cloud")
         exit(1)
-
-    for device in response['devices']:
+    return_json = response.json()
+    for device in return_json['devices']:
         measurement = device['measurement']
         id = measurement['idx']
         m_dict = {}
@@ -70,7 +132,18 @@ while True:
         else :
             type = 'unknown'
 
-        es.index(es_index,type,m_dict,id=id,overwrite_existing=True,timestamp=m_dict['@timestamp'])
-        print('data pushed to ES ')
+
+        print('data pushed to DB and MQTT ')
         print(m_dict)
     #print("finished.... sleeping")
+
+
+# this is the standard boilerplate that calls the main() function
+if __name__ == '__main__':
+    # sys.exit(main(sys.argv)) # used to give a better look to exists
+    args = parseTheArgs()
+    config = configparser.ConfigParser()
+    config.read(args.f)
+
+    rtcode = main(config)
+    sys.exit(rtcode)
